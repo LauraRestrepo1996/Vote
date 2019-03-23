d134f50d252a14ff2fed3ec2d783134e515d2c4a
LAURA MARITZA RESTREPO MU�OZ <laurarestrepo220579@correo.itm.edu.co>
2019-03-23 15:35:38 -0500

    API Events

---
+++ Vote.Web/Data/API/EventsController.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Vote.Web.Data.API
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[Controller]")]
+
+    public class EventsController : Controller
+    {
+        private readonly IEventRepository eventRepository;
+
+        public EventsController(IEventRepository eventRepository)
+        {
+            this.eventRepository = eventRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetEvents()
+        {
+            return this.Ok(this.eventRepository.GetAll());
+        }
+    }
+}
